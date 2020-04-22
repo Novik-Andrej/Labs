60d541c5f755ea7fa989f5e7466f5f2c878ca225
Andrej <andrejnovik10@gmail.com>
2020-04-22 18:22:23 +0300

    Initial commit

---
+++ lab3task1.cpp
@@ -0,0 +1,73 @@
+﻿#include<string>
+#include<fstream>
+#include<vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "ru");
+	string path = "myFile.txt";
+	ofstream file;
+	file.open(path); //не удаление предыдущих записей ofstream::app
+
+	const int n = 7;
+	int array[n];
+	if (!file.is_open()) cout << "Error\n";
+	else
+	{
+		cout << "Данные записанные в файл: ";
+		for (int i = 0; i < n; i++) {
+			array[i] = rand() % 10;
+			file << array[i] << " ";
+			cout << array[i] << " ";
+		}
+		cout << endl;
+	}
+	file.close();
+
+
+	ifstream fin;
+	fin.open(path);
+	if (!fin.is_open()) cout << "Error\n";
+	else
+	{
+		char ch;
+		vector<char> myVector;
+		cout << "Данные взятые из файла: ";
+		while (fin.get(ch))
+			if (ch != ' ')
+				myVector.push_back(ch);
+
+		for (int i = 0; i < myVector.size(); i++)
+			cout << myVector[i] << " ";
+
+		cout << endl;
+		cout << "Данные после сортировки пузырьком: ";
+
+		for (int i = 0; i < myVector.size(); i++) {
+			for (int j = 0; j < myVector.size() - 1; j++) {
+				if (myVector[j] > myVector[j + 1]) {
+					int t = myVector[j];
+					myVector[j] = myVector[j + 1];
+					myVector[j + 1] = t;
+				}
+			}
+		}
+		for (int i = 0; i < myVector.size(); i++)
+			cout << myVector[i] << " ";
+
+		file.open(path, ofstream::app);
+		file << endl;
+		if (!file.is_open()) cout << "Error\n";
+		for (int i = 0; i < myVector.size(); i++) {
+			file << myVector[i] << " ";
+		}
+		file.close();
+	}
+	fin.close();
+	return 0;
+}
+
+
+
---
+++ lab3task3.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+using namespace std;
+int main()
+{
+	setlocale(0, "ru");
+	ifstream fin;
+	fin.open("File.txt");
+	if (!fin.is_open()) cout << "Error\n";
+	else
+	{
+		string str;
+		vector<string>myVector;
+		int number = 0;
+		while (!fin.eof())
+		{
+			str = "";
+			getline(fin, str);
+			myVector.push_back(str);
+			cout << str << endl;
+			number++;
+		}
+		fin.close();
+		cout << "Количество строк: " << number << endl;	
+		int s;
+		cout << "После какой строки ставить пробел: ";
+		cin >> s;
+		if (s >= number) {
+			cout << endl;
+			cout << "Недостаточное кол-во строк" << endl;
+		}
+		else
+		{
+			if (s < number)
+			{
+				for (int i = 0; i < myVector.size(); i++) {
+					if (i == s)
+						myVector.emplace(myVector.begin() + s, "\n");
+				}
+			}
+			for (int i = 0; i < myVector.size(); i++)
+				cout << myVector[i] << endl;
+			ofstream fout;
+			fout.open("File.txt");
+			for (int i = 0; i < myVector.size(); i++) {
+				if (i == s)
+					fout << myVector[i];
+				else if (i == myVector.size() - 1)
+					fout << myVector[i];
+				else
+					fout << myVector[i] << endl;
+			}
+				fout.close();
+		}
+	}
+	return 0;
+}
